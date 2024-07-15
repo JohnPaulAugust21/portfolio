bbe4bd9b39d5ad013530f347b9293cc6246aa120
august <johnpaulaugustmaldo@gmail.com>
2024-07-15 17:49:44 +0800

    style: finalized mobile responsiveness in the Footer section.

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,15 +1,42 @@
-import React from 'react'
-import './Footer.scss'
+import React from "react";
+import "./Footer.scss";
 
 const Footer = () => {
   return (
-    <footer id='contact'>
+    <footer id="contact">
+      <div className="footer">
+        <div className="logo">
+          <img src="/uuuu.png" alt="august." />
+        </div>
 
+        <div className="contact">
+          <h1 className="city">Western Bicutan, Taguig City</h1>
 
-    <div className='footer'></div>
+          <h1 className="area">Metro Manila</h1>
+
+          <h1 className="country">1630 Philippines</h1>
+
+          <h1 className="number">+63 (920) 662 2462</h1>
+        </div>
+
+        <div className="social">
+          {" "}
+          <a
+            href="https://www.facebook.com/johnpaulaugust.maldo"
+            target="_blank"
+          >
+            <img src="/facebook.png" alt="facebook logo" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/john-paul-august-maldo-2183ab187/"
+            target="_blank"
+          >
+            <img src="/linkedin.png" alt="linkedin logo" />
+          </a>
+        </div>
+      </div>
     </footer>
+  );
+};
 
-  )
-}
-
-export default Footer
+export default Footer;
